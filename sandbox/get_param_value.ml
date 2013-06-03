30d9d1432cd626ad0369f9fd589b7d133f4ecf3d
Martial Bornet <martial-externe.bornet@edf.fr>
2013-06-03 19:40:16 +0200

    Tatonnements pour la mise au point

--- sandbox/get_param_value.ml
+++ sandbox/get_param_value.ml
@@ -6,7 +6,7 @@ open Sys ;;
 let pattern =
      if Array.length Sys.argv != 3 then
           begin
-               print_endline "Usage: get_param_value parameter_name file";
+               print_endline "(1) Usage: get_param_value parameter_name file";
                exit 1
           end
      else
@@ -20,24 +20,32 @@ let get_first l = match l with
      | [] -> ""
      | head::tail ->head ;;
 
-let process_line l =
+let ref_found = ref false ;;
+
+let process_line l ref_found =
      try let _ = Str.search_forward pattern l 0 in
           begin
                (*print_endline l ; *)
+               ref_found   := true ;
+               printf "found = %s\n" (if !ref_found then "true" else "false");
                let parts    = Str.split (regexp ":") l in
                let values   = left_shift parts in
                let value    = get_first values in
-               let qty      = Str.split (regexp " +") value in
+               let qty      = Str.split (regexp "[ \t]+") value in
                let mem_size = get_first qty in
-               printf "RAM size = [%s]\n" mem_size ;
-               exit 0 ;
+               (* printf "RAM size = [%s]\n" mem_size ; *)
+               mem_size;
+               (* exit 0 ; *)
           end
-     with Not_found -> () ;;
+     with Not_found -> "" ;;
 
 let process_chan c =
-     try while true
+     let ref_found = ref false in
+     try while !ref_found = false
           do
-               process_line (input_line c)
+               let v = process_line (input_line c) ref_found in v;
+               printf "V = %s\n" v;
+               printf "FOUND = %s\n" (if !ref_found then "true" else "false");
           done
      with End_of_file -> () ;;
 
@@ -51,5 +59,5 @@ let () =
      if Array.length Sys.argv  = 3 then
           process_file Sys.argv.(2)
      else
-          print_endline "Usage : get_param_value name file" ;
+          print_endline "(2) Usage : get_param_value name file" ;
           exit 1 ;;
