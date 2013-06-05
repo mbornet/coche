256e6e5b3f7fbaa8d5d061f8ab14b0f24b11e0d0
Martial Bornet <martial-externe.bornet@edf.fr>
2013-06-05 19:34:23 +0200

    Epuration du message de sortie

--- sandbox/get_param_value.ml
+++ sandbox/get_param_value.ml
@@ -46,24 +46,11 @@ let with_file file fct =
           exit 1
      end ;;
 
-let process_line l ref_found =
-     try let _ = Str.search_forward pattern l 0 in
-          begin
-               ref_found   := true ;
-               let parts    = Str.split (Str.regexp ":") l in
-               let values   = left_shift parts in
-               let value    = get_first values in
-               let qty      = Str.split (Str.regexp "[ \t]+") value in
-               let mem_size = get_first qty in
-               mem_size;
-          end
-     with Not_found -> "" ;;
-
 let () =
      if Array.length Sys.argv  = 3 then
           begin
                let pvalue = with_file Sys.argv.(2) search_param
-               in printf "pvalue = %s\n" pvalue
+               in printf "%s\n" pvalue
           end
      else
           print_endline "(2) Usage : get_param_value name file" ;
