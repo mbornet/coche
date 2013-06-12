d5b9e29d3c05ee3f766492b20738891da1d87b76
Martial Bornet <martial-externe.bornet@edf.fr>
2013-06-12 18:58:00 +0200

    Suppression des ';;' inutiles

--- sandbox/params.ml
+++ sandbox/params.ml
@@ -1,7 +1,7 @@
 
-open Printf ;;
+open Printf
 
-let regcomp = Str.regexp "<VOID>" ;;
+let regcomp = Str.regexp "<VOID>"
 
 (* Extract the first field of the parameter value *)
 let extract_value l =
@@ -18,7 +18,7 @@ let rec search_param_chan chan regcomp =
           try let _ = Str.search_forward regcomp line 0 in
                extract_value line
           with Not_found -> search_param_chan chan regcomp
-     with End_of_file -> "" ;;
+     with End_of_file -> ""
 
 (* Count the number of occurences of the specified regular experience *)
 let rec count_param_chan chan regcomp =
@@ -26,7 +26,7 @@ let rec count_param_chan chan regcomp =
           try let _ = Str.search_forward regcomp line 0 in
                1 + count_param_chan chan regcomp
           with Not_found -> count_param_chan chan regcomp
-     with End_of_file -> 0 ;;
+     with End_of_file -> 0
 
 let rec store chan regcomp ref_h =
      try let line = input_line chan in
@@ -34,11 +34,11 @@ let rec store chan regcomp ref_h =
                Hashtbl.replace !ref_h line true;
                store chan regcomp ref_h;
           with Not_found -> store chan regcomp ref_h ;
-     with End_of_file -> Hashtbl.length !ref_h ;;
+     with End_of_file -> Hashtbl.length !ref_h
 
 let count_uniq_chan chan regcomp =
      let h = Hashtbl.create 16 in
-          store chan regcomp (ref h) ;;
+          store chan regcomp (ref h)
 
 (* Run "fct" on "file" *)
 let with_file file fct regcomp =
@@ -50,17 +50,17 @@ let with_file file fct regcomp =
      else begin
           eprintf "File \"%s\" not found !\n" file;
           exit 1
-     end ;;
+     end
 
 let extract param_name file =
      let regcomp = Str.regexp ("^" ^ param_name ^ "[ \t:]") in
-          with_file file search_param_chan regcomp ;;
+          with_file file search_param_chan regcomp
 
 let count param_name file =
      let regcomp = Str.regexp ("^" ^ param_name ^ "[ \t:]") in
-          with_file file count_param_chan regcomp ;;
+          with_file file count_param_chan regcomp
 
 let count_uniq param_name file =
      let regcomp = Str.regexp ("^" ^ param_name ^ "[ \t:]") in
-          with_file file count_uniq_chan regcomp ;;
+          with_file file count_uniq_chan regcomp
      
