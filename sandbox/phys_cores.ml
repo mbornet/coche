fd9a92423ae3c3ba0b1b2c37801d58750bce9aee
Martial Bornet <martial-externe.bornet@edf.fr>
2013-06-13 16:50:01 +0200

    Ajout du programme "phys_cores"

---
+++ sandbox/phys_cores.ml
@@ -0,0 +1,14 @@
+
+open Printf ;;
+
+let param_cores_per_cpu  = "cpu cores"
+let param_cpu_slots      = "physical id"
+
+let file_cpuinfo         = "/proc/cpuinfo" ;;
+
+let nb_cores_per_cpu     = int_of_string (Params.extract param_cores_per_cpu file_cpuinfo) in
+let nb_cpu_slots         = Params.count_uniq param_cpu_slots  file_cpuinfo  in
+let nb_phys_cores        = nb_cores_per_cpu * nb_cpu_slots in
+     printf "%d\n" nb_phys_cores ;
+
+exit 0
