778ce3818486b0fa337bde16815ca710175e38a9
Martial Bornet <martial-externe.bornet@edf.fr>
2013-06-18 18:34:06 +0200

    Controle des arguments de file_uid

--- sandbox/file_uid.ml
+++ sandbox/file_uid.ml
@@ -2,6 +2,15 @@
 open Unix ;;
 open Printf ;;
 
-let uid = Unix.stat Sys.argv.(1) in
-     printf "UID = %d\n" uid.st_uid ;;
+let print_uid file =
+     if Sys.file_exists file then
+          let uid = Unix.stat file in
+               printf "%d\n" uid.st_uid 
+     else
+          eprintf "\"%s\" : no such file or directory\n" file ;;
 
+let nb_files = Array.length Sys.argv - 1 in
+     if nb_files > 0 then
+          print_uid Sys.argv.(1)
+     else
+          eprintf "Usage: %s pathname\n" Sys.argv.(0) ;;
