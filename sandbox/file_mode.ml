d52fadb84f7076c757df3f0a71214c8e86f6a112
Martial Bornet <martial-externe.bornet@edf.fr>
2013-06-19 09:49:39 +0200

    Controle des arguments de file_mode + modification du format d'affichage du resultat

--- sandbox/file_mode.ml
+++ sandbox/file_mode.ml
@@ -2,6 +2,15 @@
 open Unix ;;
 open Printf ;;
 
-let mode = Unix.stat Sys.argv.(1) in
-     printf "MODE = %o\n" mode.st_perm ;;
+let print_mode file =
+     if Sys.file_exists file then
+          let mode = Unix.stat Sys.argv.(1) in
+               printf "%04o\n" mode.st_perm
+     else
+          eprintf "\"%s\" : no such file or directory\n" file ;;
 
+let nb_files = Array.length Sys.argv - 1 in
+     if nb_files > 0 then
+          print_mode Sys.argv.(1)
+     else
+          eprintf "Usage: %s pathname\n" Sys.argv.(0) ;;
