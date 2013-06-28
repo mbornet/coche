082eaacd8f79c708a4bb844d7c637a7298e6d4e1
Martial Bornet <martial-externe.bornet@edf.fr>
2013-06-28 15:53:13 +0200

    Simplification du code. printf pas systematique. Correction dans Makefile

--- sandbox/cpu_max_freq.ml
+++ sandbox/cpu_max_freq.ml
@@ -1,9 +1,10 @@
 
-open Printf ;;
+open Printf
 
-let param_cpu_max_freq = "model name" ;;
-let file_cpu_max_freq  = "/proc/cpuinfo" ;;
+let param_cpu_max_freq = "model name"
+let file_cpu_max_freq  = "/proc/cpuinfo"
 
-let cpu_max_freq = Params.last_field param_cpu_max_freq file_cpu_max_freq in
-     printf "%s\n" cpu_max_freq;
+let cpu_max_freq = Params.last_field param_cpu_max_freq file_cpu_max_freq
+
+let _ = printf "%s\n" cpu_max_freq
 
