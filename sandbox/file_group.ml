f366722b65758262f76dc7dfe178c60f92411b76
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-02 18:15:03 +0200

    Uniformisation du code avec les autres programmes

--- sandbox/file_group.ml
+++ sandbox/file_group.ml
@@ -5,8 +5,8 @@ open Printf
 let group file =
   if Sys.file_exists file then
     let gid_st = Unix.stat file in
-        let gr = getgrgid gid_st.st_gid in
-          gr.gr_name
+      let gr = getgrgid gid_st.st_gid in
+        gr.gr_name
   else
     raise Not_found
 
@@ -15,6 +15,6 @@ let _ = let nb_files = Array.length Sys.argv - 1 in
     let file = Sys.argv.(1) in
     try
       printf "%s\n" (group file)
-    with Not_found -> eprintf "ERROR: \"%s\" not found !\n" file
+    with Not_found -> eprintf "\"%s\" : no such file or directory\n" file
   else
     eprintf "Usage: %s pathname\n" Sys.argv.(0)
