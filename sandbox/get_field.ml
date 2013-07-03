0d35d991c64612b38993915e1cccc7714d701682
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-03 08:04:37 +0200

    Menage dans le code

--- sandbox/get_field.ml
+++ sandbox/get_field.ml
@@ -7,5 +7,6 @@ let get_field field_no line =
      let result  = List.nth fields field_no in
           result ;;
 
-let version = get_field 2 "Linux version 2.6.32-5-amd64 (Debian 2.6.32-48squeeze3) (dannf@debian.org) (gcc version 4.3.5 (Debian 4.3.5-4) ) #1 SMP Fri May 10 08:43:19 UTC 2013" in
-     printf "%s\n" version
+let version = get_field 2 "Linux version 2.6.32-5-amd64 (Debian 2.6.32-48squeeze3) (dannf@debian.org) (gcc version 4.3.5 (Debian 4.3.5-4) ) #1 SMP Fri May 10 08:43:19 UTC 2013"
+
+let _ = printf "%s\n" version
--- sandbox/hyperthreading.ml
+++ sandbox/hyperthreading.ml
@@ -1,5 +1,5 @@
 
-open Printf ;;
+open Printf
 
 let param_cores_per_cpu  = "cpu cores"
 let param_cpu_slots      = "physical id"
@@ -9,17 +9,12 @@ let file_cpuinfo         = "/proc/cpuinfo"
 
 let nb_cores_per_cpu     = int_of_string (Params.extract param_cores_per_cpu file_cpuinfo)
 let nb_cpu_slots         = Params.count_uniq param_cpu_slots  file_cpuinfo
-let nb_virt_procs        = Params.count      param_virt_procs file_cpuinfo ;;
-
-(*
-printf "Nb cores per CPU      = %d\n" nb_cores_per_cpu ;
-printf "Nb CPU slots          = %d\n" nb_cpu_slots     ;
-printf "Nb virtual processors = %d\n" nb_virt_procs    ;
-*)
+let nb_virt_procs        = Params.count      param_virt_procs file_cpuinfo
 
 let ht_status =
-     if nb_virt_procs = (nb_cores_per_cpu * nb_cpu_slots)     then "off" else
-     if nb_virt_procs = (nb_cores_per_cpu * nb_cpu_slots * 2) then "on"  else
-     "UNKNOWN"
-     in printf "%s\n" ht_status ;
+          if nb_virt_procs = (nb_cores_per_cpu * nb_cpu_slots)     then "off" else
+          if nb_virt_procs = (nb_cores_per_cpu * nb_cpu_slots * 2) then "on"  else
+          "UNKNOWN"
+
+let _ = printf "%s\n" ht_status
 
