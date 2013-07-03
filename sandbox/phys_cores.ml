8d27d179a9404c16cbf1dd53f2c499be72612f3c
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-03 15:51:07 +0200

    Remaniement du code

--- sandbox/phys_cores.ml
+++ sandbox/phys_cores.ml
@@ -1,13 +1,14 @@
 
-open Printf ;;
+open Printf
 
 let param_cores_per_cpu  = "cpu cores"
 let param_cpu_slots      = "physical id"
 
-let file_cpuinfo         = "/proc/cpuinfo" ;;
+let file_cpuinfo         = "/proc/cpuinfo"
 
-let nb_cores_per_cpu     = int_of_string (Params.extract param_cores_per_cpu file_cpuinfo) in
-let nb_cpu_slots         = Params.count_uniq param_cpu_slots  file_cpuinfo  in
-let nb_phys_cores        = nb_cores_per_cpu * nb_cpu_slots in
-     printf "%d\n" nb_phys_cores ;
+let nb_phys_cores = let nb_cores_per_cpu = int_of_string (Params.extract param_cores_per_cpu file_cpuinfo) in
+     let nb_cpu_slots = Params.count_uniq param_cpu_slots file_cpuinfo in
+          nb_cores_per_cpu * nb_cpu_slots
+
+let _ = printf "%d\n" nb_phys_cores
 
--- sandbox/virtual_cores.ml
+++ sandbox/virtual_cores.ml
@@ -1,10 +1,11 @@
 
-open Printf ;;
+open Printf
 
 let param_virt_procs     = "processor"
 
-let file_cpuinfo         = "/proc/cpuinfo" ;;
+let file_cpuinfo         = "/proc/cpuinfo"
 
-let nb_virt_procs        = (Params.count param_virt_procs file_cpuinfo) in
-     printf "%d\n" nb_virt_procs ;;
+let nb_virt_procs        = (Params.count param_virt_procs file_cpuinfo)
+
+let _ = printf "%d\n" nb_virt_procs
 
