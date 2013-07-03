aeb58cd80674b268c5c3e0626b83f6763c2b9458
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-03 08:07:51 +0200

    Suppression des ";;" inutiles

--- sandbox/lists.ml
+++ sandbox/lists.ml
@@ -3,16 +3,16 @@ open Printf
 
 let left_shift l = match l with
      | [] -> l
-     | head::tail -> tail ;;
+     | head::tail -> tail
 
 let get_first l = match l with
      | [] -> ""
-     | head::tail -> head ;;
+     | head::tail -> head
 
 let get_last l =
      let lg = List.length l in
-     List.nth l (lg - 1) ;;
+     List.nth l (lg - 1)
 
 let rec print_list l = match l with
      | [] -> printf "\n"
-     | head::tail -> printf "%s " head ; print_list tail ;;
+     | head::tail -> printf "%s " head ; print_list tail
