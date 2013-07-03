324d0f8b726ab5800c63c6ec4f27d87959b6a631
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-03 08:09:19 +0200

    Menage dans le code

--- sandbox/mem_size.ml
+++ sandbox/mem_size.ml
@@ -1,9 +1,10 @@
 
-open Printf ;;
+open Printf
 
-let param_mem_size      = "MemTotal" ;;
-let file_mem_size       = "/proc/meminfo" ;;
+let param_mem_size      = "MemTotal"
+let file_mem_size       = "/proc/meminfo"
 
-let mem_size = Params.extract param_mem_size file_mem_size in
-     printf "%s\n" mem_size ;
+let mem_size = Params.extract param_mem_size file_mem_size
+
+let _ = printf "%s\n" mem_size
 
