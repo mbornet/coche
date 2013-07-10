a9f01b1f7f1ea0418dd3b2481a6057ab654a9a7c
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-10 17:27:27 +0200

    Ajout de l'interface params.mli

---
+++ sandbox/params.mli
@@ -0,0 +1,22 @@
+
+val extract_value          : string -> string
+val extract_last_value     : string -> string
+val search_param_chan      : in_channel -> Str.regexp -> string
+val search_param_last_chan : in_channel -> Str.regexp -> string
+
+val count_param_chan       : in_channel -> Str.regexp -> int
+val store_uniq             : in_channel -> Str.regexp -> (string, bool) Hashtbl.t ref -> int
+val count_uniq_chan        : in_channel -> Str.regexp -> int
+
+val get_field              : int -> string -> string
+val get_field_chan         : in_channel -> int -> string
+
+val with_file              : string -> (in_channel -> Str.regexp -> string) -> Str.regexp -> string
+val mk_re_param            : string -> Str.regexp
+
+val extract                : string -> string -> string
+val count                  : string -> string -> int
+val count_uniq             : string -> string -> int
+val field                  : int -> string -> string
+val last_field             : string -> string -> string
+
