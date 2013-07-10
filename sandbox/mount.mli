d15ba7bbcfbfba3c8c62ad3235b56821f7a4736a
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-10 18:27:07 +0200

    Complements a l'interface mount.mli

--- sandbox/mount.mli
+++ sandbox/mount.mli
@@ -1,4 +1,5 @@
 
-val chan_to_list : in_channel -> string list
+val print_status     : Unix.process_status -> unit
+val chan_to_list     : in_channel -> string list
 val run_proc_to_list : string -> string list
 
