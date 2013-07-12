946e22191f7ae307713a83f118c758b725150636
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-12 18:32:54 +0200

    Premiere version de mount gerant une structure avec les differents attributs d'un mount

--- sandbox/mount.ml
+++ sandbox/mount.ml
@@ -36,11 +36,35 @@ let _ = let result = run_proc_to_list "/bin/mount" in
 let _ = let result = run_proc_to_list "/bin/mount" in
   Lists.print_num_str_list_nl result 1
 
-let param_name = "/dev/sdc1"
 
-let s =
-  let regcomp = Str.regexp ( "^" ^ param_name ^ "[ \t]") in
-    with_proc "/bin/mount" Params.search_param_last_field_chan regcomp
+(* field numbers of the attributes of a device mount *)
+let mount_dev_idx         = 1 - 1
+let mount_mount_point_idx = 3 - 1
+let mount_fs_type_idx     = 5 - 1
+let mount_options_idx     = 6 - 1
 
-let _ = printf "%s\n" s
+let mount_store_params params_list =
+  let params_array = Array.of_list params_list in
+    let m = {
+      dev_st         = params_array.(mount_dev_idx);
+      mount_point_st = params_array.(mount_mount_point_idx);
+      fs_type_st     = params_array.(mount_fs_type_idx);
+      options_st     = params_array.(mount_options_idx)
+    }
+    in m
+
+let rec set_mount_attr chan regcomp =
+  try let line = input_line chan in
+    try let _ = Str.search_forward regcomp line 0 in
+      let fields = Str.split (Str.regexp "[ \t]+") line in
+        mount_store_params fields
+    with Not_found -> set_mount_attr chan regcomp
+  with End_of_file -> raise Not_found
+
+let s device_name =
+  let regcomp = Str.regexp ( "^" ^ device_name ^ "[ \t]") in
+    with_proc "/bin/mount" set_mount_attr regcomp
+
+let _ = let m = s "/dev/sda1" in
+    printf "%s %s\n" m.dev_st m.mount_point_st
 
