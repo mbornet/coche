30c23a52eb2d0e6c761ed24296a493d76d406f24
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-16 14:38:36 +0200

    Ajout du fichier lists.mli

--- sandbox/lists.mli
+++ sandbox/lists.mli
@@ -4,6 +4,8 @@ val get_first             : string list -> string
 val get_last              : string list -> string
 (*
 val get_last              : 'a list -> 'a
+val chan_to_list     : in_channel -> string list
+val run_proc_to_list : string -> string list
 *)
 val print_str_list        : string list -> unit
 val print_str_list_nl     : string list -> unit
