925c004cba4d4f4e929526b44cea36ba35d4cfde
Martial Bornet <martial-externe.bornet@edf.fr>
2013-07-16 17:38:15 +0200

    Suppression de warning avec ignore()

--- sandbox/ip.ml
+++ sandbox/ip.ml
@@ -14,11 +14,11 @@ let ip_cmd = "/sbin/ifconfig eth0"
 let with_cmd cmd fct regcomp  =
   let chan = Unix.open_process_in cmd in
     try let res = fct chan regcomp in
-      Unix.close_process_in chan; res
-    with e -> Unix.close_process_in chan; raise e
+       ignore (Unix.close_process_in chan); res
+    with e -> ignore (Unix.close_process_in chan); raise e
 
-let get_ip_addr = let regcomp = Str.regexp ("inet addr:") in
+let get_ip_addr () = let regcomp = Str.regexp ("inet addr:") in
   with_cmd ip_cmd Params.search_param_chan regcomp ;;
 
-let ip_addr = get_ip_addr in
+let ip_addr = get_ip_addr () in
     printf "%s\n" ip_addr
