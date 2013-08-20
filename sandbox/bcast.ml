3ea62caf2aa6435a68103ccb37ae61b3c29eaed6
Martial Bornet <martial-externe.bornet@edf.fr>
2013-08-20 16:37:00 +0200

    Mise au point de bcast.ml

--- sandbox/bcast.ml
+++ sandbox/bcast.ml
@@ -1,6 +1,7 @@
 
 open Printf
 
+(*
 type ip_st =
   {
     ip_st         : string;
@@ -8,8 +9,10 @@ type ip_st =
     netmask_st    : string;
     mac_st        : string;
     state_st      : string }
+*)
 
 let ip_cmd = "/sbin/ifconfig eth0"
+let regex  = "^.*[ \t]+Bcast:\\([1-9][0-9.]+\\)"
 
 let with_cmd cmd fct regcomp  =
   let chan = Unix.open_process_in cmd in
@@ -17,8 +20,9 @@ let with_cmd cmd fct regcomp  =
        ignore (Unix.close_process_in chan); res
     with e -> ignore (Unix.close_process_in chan); raise e
 
-let get_bcast_addr () = let regcomp = Str.regexp ("^.*[ \t]+Bcast:") in
-  with_cmd ip_cmd Params.get_RE_param_chan regcomp ;;
+let get_bcast_addr () =
+  let regcomp = Str.regexp regex in
+    with_cmd ip_cmd Params.get_RE_param_chan regcomp ;;
 
 let bcast_addr = get_bcast_addr () in
     printf "%s\n" bcast_addr
