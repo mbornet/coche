1d964f8b651e402a290b058a0e6a7c593194f91b
Martial Bornet <martial-externe.bornet@edf.fr>
2013-09-03 17:43:17 +0200

    Utilisation du module lists.ml dans rec_list_copy.ml

--- sandbox/rec_list_copy.ml
+++ sandbox/rec_list_copy.ml
@@ -2,12 +2,13 @@
 
 open Printf
 
+(*
 let rec print_str_list l = match l with
      | [] -> printf "\n"
      | head::tail -> printf "%s " head ; print_str_list tail
 
 let print_list = print_str_list
-
+*)
 
 
 let rec list_cp l = match l with
@@ -16,8 +17,7 @@ let rec list_cp l = match l with
 
 let l1 = "a" :: "b" :: "c" ::[] ;;
 
-(* let _ = Lists.print_list l1 *)
-let _ = print_list l1
+let _ = Lists.print_list l1
 
 let l2 = list_cp l1
 
